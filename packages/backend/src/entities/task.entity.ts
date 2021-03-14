1dcffacfabc2a3afa458c27b89e19f96338bf8d4
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-03-14 16:23:07 +0900

    task model

---
+++ packages/backend/src/entities/task.entity.ts
@@ -0,0 +1,37 @@
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Task {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({ nullable: true })
+	content: string;
+
+	@Column({ nullable: true })
+	category: number;
+
+	@Column()
+	sectionId: number;
+
+	@Column()
+	order: number;
+
+	@Column()
+	status: number;
+
+	@Column()
+	createdBy: string;
+
+	@CreateDateColumn()
+	createdAt: string;
+
+	@UpdateDateColumn()
+	updatedAt: string;
+}
