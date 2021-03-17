1800448134670f9dfd5403f016a24356449fe24b
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-03-17 21:03:03 +0900

    test environment setup

---
+++ packages/frontend/src/pages/project.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "../test-utils";
+import Project from "./project";
+
+describe("test", () => {
+	it("should render the test", () => {
+		render(<Project />);
+		const heading = screen.getByText("helloTest");
+		expect(heading).toBeInTheDocument();
+	});
+});
--- packages/frontend/src/pages/project.tsx
+++ packages/frontend/src/pages/project.tsx
@@ -6,6 +6,7 @@ const Project = () => {
 	const { data, loading } = useQuery(GET_TASKS);
 	return (
 		<div className="container">
+			<div>helloTest</div>
 			<div>{data && data.getTasks.map(task => <div>{task.content}</div>)}</div>
 		</div>
 	);
