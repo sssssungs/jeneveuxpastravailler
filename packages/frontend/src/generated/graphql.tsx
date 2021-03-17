61c56a4430abc32e3d0dc313561a563ef51eb454
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-03-17 20:51:34 +0900

    graphql codegen setup

---
+++ packages/frontend/src/generated/graphql.tsx
@@ -0,0 +1,135 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type TaskDto = {
+  __typename?: 'TaskDto';
+  id: Scalars['Float'];
+  content: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  getTasks: Array<TaskDto>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTask: TaskDto;
+};
+
+
+export type MutationCreateTaskArgs = {
+  newData: TaskInput;
+};
+
+export type TaskInput = {
+  content: Scalars['String'];
+};
+
+export type CreateTaskMutationVariables = Exact<{
+  content: Scalars['String'];
+}>;
+
+
+export type CreateTaskMutation = (
+  { __typename?: 'Mutation' }
+  & { createTask: (
+    { __typename?: 'TaskDto' }
+    & Pick<TaskDto, 'id' | 'content'>
+  ) }
+);
+
+export type GetTasksQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetTasksQuery = (
+  { __typename?: 'Query' }
+  & { getTasks: Array<(
+    { __typename?: 'TaskDto' }
+    & Pick<TaskDto, 'id' | 'content'>
+  )> }
+);
+
+
+export const CreateTaskDocument = gql`
+    mutation CreateTask($content: String!) {
+  createTask(newData: {content: $content}) {
+    id
+    content
+  }
+}
+    `;
+export type CreateTaskMutationFn = Apollo.MutationFunction<CreateTaskMutation, CreateTaskMutationVariables>;
+
+/**
+ * __useCreateTaskMutation__
+ *
+ * To run a mutation, you first call `useCreateTaskMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateTaskMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createTaskMutation, { data, loading, error }] = useCreateTaskMutation({
+ *   variables: {
+ *      content: // value for 'content'
+ *   },
+ * });
+ */
+export function useCreateTaskMutation(baseOptions?: Apollo.MutationHookOptions<CreateTaskMutation, CreateTaskMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateTaskMutation, CreateTaskMutationVariables>(CreateTaskDocument, options);
+      }
+export type CreateTaskMutationHookResult = ReturnType<typeof useCreateTaskMutation>;
+export type CreateTaskMutationResult = Apollo.MutationResult<CreateTaskMutation>;
+export type CreateTaskMutationOptions = Apollo.BaseMutationOptions<CreateTaskMutation, CreateTaskMutationVariables>;
+export const GetTasksDocument = gql`
+    query GetTasks {
+  getTasks {
+    id
+    content
+  }
+}
+    `;
+
+/**
+ * __useGetTasksQuery__
+ *
+ * To run a query within a React component, call `useGetTasksQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetTasksQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetTasksQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetTasksQuery(baseOptions?: Apollo.QueryHookOptions<GetTasksQuery, GetTasksQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetTasksQuery, GetTasksQueryVariables>(GetTasksDocument, options);
+      }
+export function useGetTasksLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetTasksQuery, GetTasksQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetTasksQuery, GetTasksQueryVariables>(GetTasksDocument, options);
+        }
+export type GetTasksQueryHookResult = ReturnType<typeof useGetTasksQuery>;
+export type GetTasksLazyQueryHookResult = ReturnType<typeof useGetTasksLazyQuery>;
+export type GetTasksQueryResult = Apollo.QueryResult<GetTasksQuery, GetTasksQueryVariables>;
--- packages/frontend/src/pages/project.tsx
+++ packages/frontend/src/pages/project.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/client";
 import { GET_TASKS } from "../graphql/task/query/getTasks";
 
 const Project = () => {
-	const { data } = useQuery(GET_TASKS);
+	const { data, loading } = useQuery(GET_TASKS);
 	return (
 		<div className="container">
 			<div>{data && data.getTasks.map(task => <div>{task.content}</div>)}</div>
