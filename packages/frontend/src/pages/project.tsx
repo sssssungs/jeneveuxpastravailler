fc588653840a41f3ee9dad2beafa82c9f6c14573
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-03-24 22:04:40 +0900

    Added gssp with apollo

--- packages/frontend/src/pages/project.tsx
+++ packages/frontend/src/pages/project.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import CommonLayout from "../components/common/commonLayout";
-import { useQuery } from "@apollo/client";
 import { GET_TASKS } from "../graphql/task/query/getTasks";
+import { addApolloState, initializeApollo } from "apollo";
+import { TaskDto } from "generated/graphql";
+import TaskCard from "components/project/taskCard";
 
-const Project = () => {
-	// const { data, loading } = useQuery(GET_TASKS);
+const Project = initialState => {
+	const { data } = initialState;
 	return (
 		<CommonLayout current={"project"}>
-			<div className="container">
-				<div>helloTest</div>
-			</div>
+			{data.getTasks?.map(task => (
+				<TaskCard task={task} />
+			))}
 		</CommonLayout>
 	);
 };
 
 export default Project;
+
+export const getServerSideProps = async () => {
+	const apolloClient = initializeApollo();
+	const { data } = await apolloClient.query({ query: GET_TASKS });
+	return addApolloState<TaskDto[]>(apolloClient, {
+		props: { data: data as TaskDto[] },
+	});
+};
