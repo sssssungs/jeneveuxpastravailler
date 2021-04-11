e7685385874e54e74e706efa4e936aa79e304de2
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-04-11 15:40:51 +0900

    Add section (wip)

--- packages/frontend/src/components/project/taskSection.tsx
+++ packages/frontend/src/components/project/taskSection.tsx
@@ -1,7 +1,45 @@
+import styled from '@emotion/styled';
 import * as React from 'react';
 
-const TaskSection = () => {
-	return <div>TaskSection</div>;
+interface Props {
+	children?: React.ReactNode;
+}
+
+const TaskSection = ({ children }: Props) => {
+	const [addSection, setAddSection] = React.useState<boolean>(false);
+	return (
+		<SectionLayout>
+			<TaskSectionWrapper>{children}</TaskSectionWrapper>
+			<AddInput />
+		</SectionLayout>
+	);
 };
 
 export default TaskSection;
+
+const AddInput = styled.div`
+	display: none;
+	width: 20px;
+	margin-left: ${props => props.theme.spacing.m};
+	border: 1px solid ${props => props.theme.colors.light.SHADOW};
+	border-radius: ${props => props.theme.spacing.m};
+	background-color: transparent;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+const SectionLayout = styled.div`
+	display: flex;
+	width: fit-content;
+	border: 2px red dashed;
+	&:hover {
+		${AddInput} {
+			display: block;
+		}
+	}
+`;
+
+const TaskSectionWrapper = styled.div`
+	width: fit-content;
+	background-color: yellow;
+`;
--- packages/frontend/src/pages/project.tsx
+++ packages/frontend/src/pages/project.tsx
@@ -15,16 +15,25 @@ import { Modal } from 'react-responsive-modal';
 import TaskModal from '../components/project/taskModal';
 import AddButton from '../components/project/addButton';
 import { ReactSortable } from 'react-sortablejs';
+import { useApolloClient } from '@apollo/react-hooks';
+import _cloneDeep from 'lodash/cloneDeep';
+import TaskSection from '../components/project/taskSection';
 
 const Project = () => {
 	const { data } = useGetTasksQuery();
+	const ac = useApolloClient();
+	// const [data, setData] = React.useState(data);
 	const [modalOpen, setModalOpen] = React.useState<boolean>(false);
 	const [content, setContent] = React.useState<string>('');
 	const [createTaskMutation] = useCreateTaskMutation({
 		variables: { content, sectionId: 0 },
 		refetchQueries: [{ query: GET_TASKS }],
 	});
-	const [changeTaskOrderMutation] = useChangeTaskOrderMutation();
+	const [changeTaskOrderMutation, { loading, error }] = useChangeTaskOrderMutation({
+		onCompleted: a => {
+			console.log('변경완료', a);
+		},
+	});
 	const [isDragging, setIsDragging] = React.useState<boolean>(false);
 
 	const setModal = (value: boolean) => {
@@ -53,22 +62,22 @@ const Project = () => {
 	const dragEnd = async e => {
 		setIsDragging(false);
 		const { oldIndex, newIndex } = e;
-
+		const { getTasks } = _cloneDeep(ac.readQuery({ query: GetTasksDocument }));
+		[getTasks[oldIndex], getTasks[newIndex]] = [getTasks[newIndex], getTasks[oldIndex]];
 		await changeTaskOrderMutation({
 			variables: {
 				sectionId: 0,
 				selectOrder: data.getTasks[oldIndex].order,
 				targetOrder: data.getTasks[newIndex].order,
 			},
-			refetchQueries: [{ query: GetTasksDocument }],
-			// update: (store, { data }) => {
-			// 	store.writeQuery<GetTasksQuery>({
-			// 		query: GetTasksDocument,
-			// 		data: {
-			// 			getTasks: [...data!.changeTaskOrder],
-			// 		},
-			// 	});
-			// },
+			update: (cache, { data }) => {
+				cache.writeQuery<GetTasksQuery>({
+					query: GetTasksDocument,
+					data: {
+						getTasks: data!.changeTaskOrder,
+					},
+				});
+			},
 		});
 	};
 
@@ -91,24 +100,31 @@ const Project = () => {
 					showDelete={false}
 				/>
 			</Modal>
-			<AddButton onClick={setModal} />
-			{data?.getTasks && (
-				<ReactSortable
-					group={'0'}
-					list={data?.getTasks as TaskDto[]}
-					setList={data => {}}
-					animation={300}
-					swapThreshold={0.75}
-					fallbackOnBody={true}
-					forceFallback={true}
-					onStart={dragStart}
-					onEnd={dragEnd}
-				>
-					{data?.getTasks?.map((task: TaskDto, index) => (
-						<TaskCard task={task} key={index} order={String(task.order)} isDragging={isDragging} />
-					))}
-				</ReactSortable>
-			)}
+			<TaskSection>
+				<AddButton onClick={setModal} />
+				{data?.getTasks && (
+					<ReactSortable
+						group={'0'}
+						list={data?.getTasks as TaskDto[]}
+						setList={data => {}}
+						animation={300}
+						swapThreshold={0.75}
+						fallbackOnBody={true}
+						forceFallback={true}
+						onStart={dragStart}
+						onEnd={dragEnd}
+					>
+						{data?.getTasks?.map((task: TaskDto, index) => (
+							<TaskCard
+								task={task}
+								key={index}
+								order={String(task.order)}
+								isDragging={isDragging}
+							/>
+						))}
+					</ReactSortable>
+				)}
+			</TaskSection>
 		</CommonLayout>
 	);
 };
