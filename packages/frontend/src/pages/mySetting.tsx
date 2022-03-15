833a353d8ccf7dbc6ac9a537fe8b8d02d4999db8
Randy <sssssungs@gmail.com>
2022-03-15 17:07:35 +0900

    Update mySetting.tsx

--- packages/frontend/src/pages/mySetting.tsx
+++ packages/frontend/src/pages/mySetting.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import CommonLayout from '../components/common/commonLayout';
-
+import DeleteButton from '../components/project/deleteButton';
+import { mq } from 'styles/mq';
+import _find from 'lodash/find';
 const MySetting = () => {
 	return <CommonLayout current={'mySetting'}>MySetting</CommonLayout>;
 };
