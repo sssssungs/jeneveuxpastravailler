ccd084a3d4d3639104d306acd38e31a2bd19b70c
Randy <sssssungs@gmail.com>
2022-03-15 20:33:28 +0900

    Update index.tsx

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import CommonLayout from '../components/common/commonLayout';
 
 export default function Home() {
+	
+	const example = (a) => {
+		let i = 10;
+		return i + a;
+		i++;
+	}
+	
 	return (
 		<CommonLayout current={''}>
 			<div className="container">
